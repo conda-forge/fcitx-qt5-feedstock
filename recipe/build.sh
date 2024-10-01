dd5b71ea5819290aa1b69aa0f36cdf63fbf333d4
Mark Harfouche <mark.harfouche@gmail.com>
2024-10-01 18:28:19 -0400

    Use cmake args

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,12 @@
+set -ex
 mkdir -p build
 cd build
 
-cmake -G "Ninja" \
-      -D ENABLE_LIBRARY:BOOL=OFF \
-      -D CMAKE_BUILD_TYPE=Release \
-      -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
-      -D CMAKE_PREFIX_PATH:FILEPATH=$PREFIX \
-      ..
+cmake ${CMAKE_ARGS} \
+    -G "Ninja" \
+    -D ENABLE_LIBRARY:BOOL=OFF \
+    -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
+    -D CMAKE_PREFIX_PATH:FILEPATH=$PREFIX \
+    ..
 
 ninja install
