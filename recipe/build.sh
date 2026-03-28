a31e3f747080cda80a8010f96c5f221c4230c27c
Daniel Nachun <daniel.nachun@gmail.com>
2026-03-28 01:30:08 -0700

    fix cmake

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 cmake -B build -S ${SRC_DIR} \
     -G "Ninja" \
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
     -D ENABLE_LIBRARY:BOOL=OFF \
     ${CMAKE_ARGS}
 
